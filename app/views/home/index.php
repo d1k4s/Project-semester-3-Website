1fbb6e843d1d664102c00ba667cd54bc46770c6b
sulthon <sulthon.abady28@gmail.com>
2022-10-29 00:09:07 +0700

    Contact Section

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -186,3 +186,38 @@
     </div>
 </section>
 <!-- Blog Section End -->
+
+<!-- Contact Section Start -->
+<section id="contact" class="pt-36 pb-32">
+    <div class="container">
+        <div class="w-full px-4">
+            <div class="max-w-xl mx-auto text-center mb-16">
+                <h4 class="font-semibold text-lg text-primary mb-2 sm:4xl lg:5xl">Contact</h4>
+                <h2 class="font-bold text-dark text-3xl mb-4">Hubungi Kami</h2>
+                <p class="font-medium text-md text-secondary md:text-lg">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam, at.</p>
+            </div>
+        </div>
+
+        <form action="">
+            <div class="w-full lg:w-2/3 lg:mx-auto">
+                <div class="w-full px-4 mb-8">
+                    <label for="name" class="text-base font-bold text-primary">Nama</label>
+                    <input type="text" name="name" id="name" class="w-full bg-slate-200 text-dark p-3 rounded-md focus:outline-none focus:ring-primary focus:ring-1 focus:border-primary">
+                </div>
+                <div class="w-full px-4 mb-8">
+                    <label for="email" class="text-base font-bold text-primary">Email</label>
+                    <input type="email" name="email" id="email" class="w-full bg-slate-200 text-dark p-3 rounded-md focus:outline-none focus:ring-primary focus:ring-1 focus:border-primary">
+                </div>
+                <div class="w-full px-4 mb-8">
+                    <label for="message" class="text-base font-bold text-primary">Pesan</label>
+                    <textarea type="text" name="message" id="message" class="w-full bg-slate-200 text-dark p-3 rounded-md focus:outline-none focus:ring-primary focus:ring-1 focus:border-primary h-32"></textarea>
+                </div>
+                <div class="w-full px-4">
+                    <button class="text-base font-semibold text-white bg-primary py-3 px-8 rounded-full w-full hover:opacity-80 hover:shalolg
+                    transition duration-500">Kirim</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</section>
+<!-- Contact Section End -->
